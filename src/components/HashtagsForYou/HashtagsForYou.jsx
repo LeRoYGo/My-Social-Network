769fcc790f27c53035054556abed28214351d20a
LeRoYGo <roman.lepikhin.2000@gmail.com>
2024-05-11 23:01:12 +0500

    fix

--- src/components/HashtagsForYou/HashtagsForYou.jsx
+++ src/components/HashtagsForYou/HashtagsForYou.jsx
@@ -2,7 +2,7 @@ import s from './HashtagsForYou.module.css';
 
 function HashtagsForYou() {
 	return (
-		<div className={`sidebar ${s.hashtag__list}`}>
+		<div className={s.hashtag__list}>
 			<h3 className={`${s.hashtag} ${s.title}`}>Hashtags for you</h3>
 			<div className={s.hashtag}>
 				<a href="#!">#frontend</a>
@@ -23,6 +23,11 @@ function HashtagsForYou() {
 			<div className={s.hashtag}>
 				<a href="#!">#frontend</a>
 				<span>1,222 Teams</span>
+			</div>
+			<div className={s.hashtag}>
+				<a className={s.btn_Show_More} href="#!">
+					Show more
+				</a>
 			</div>
 		</div>
 	);
