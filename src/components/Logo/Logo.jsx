46bbdfd3fefdbcb92a9b16da9ab60e01daa14f10
LeRoYGo <roman.lepikhin.2000@gmail.com>
2024-05-12 15:15:51 +0500

    fix

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,12 +1,13 @@
 import s from './Logo.module.css';
 import Logo from '/logo.svg';
+import { NavLink } from 'react-router-dom';
 
 function NavBar(props) {
 	return (
-		<div className={`${s.logo} ${props.style}`}>
+		<NavLink to="/" className={`${s.logo} ${props.style}`}>
 			<img src={Logo} alt="logo" />
 			<span className={s.logo__title}>teamder</span>
-		</div>
+		</NavLink>
 	);
 }
 
